9a1e40dd5f04f018a1b2033cfd1f0abe8fbf6b96
Thiago Leite <thiagofmleite@gmail.com>
2024-04-04 09:41:30 -0300

    Moving InputField to styled components

--- src/components/UI/Form/InputField.jsx
+++ src/components/UI/Form/InputField.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import Input from "./Input";
 import Label from "./Label";
 import Error from "../Error";
-
-import styles from "./InputField.module.css";
+import FormControl from "./FormControl";
 
 const InputField = ({ label, setValue, error, validate, ...props }) => {
   return (
-    <div className={styles.wrapper}>
-      <Label className={styles.label} htmlFor={props.name}>
-        {label}
-      </Label>
-      <Input className={styles.input} {...props} />
-      {error && <Error className={styles.erro}>{error}</Error>}
-    </div>
+    <FormControl>
+      <Label htmlFor={props.name}>{label}</Label>
+      <Input {...props} />
+      {error && <Error>{error}</Error>}
+    </FormControl>
   );
 };
 
