782fe9c0117795fb1a2f80d254734bd127182ad8
Thiago Leite <thiagofmleite@gmail.com>
2024-04-06 13:52:03 -0300

    Footer Component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import DogsFooterSvg from "../../assets/dogs-footer.svg?react";
 
 const Footer = () => {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <DogsFooterSvg />
+      <p>Alguns direitos reservados</p>
+    </footer>
+  );
 };
 
 export default Footer;
