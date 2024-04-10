2757b60c5d2cdfbb9565fa66abc43f23fa74ffa2
Thiago Leite <thiagofmleite@gmail.com>
2024-04-10 12:57:56 -0300

    Setting up Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
+import store from "./store/configureStore.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ src/store/configureStore.js
@@ -0,0 +1,15 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const contador = () => 0;
+
+const reducer = combineReducers({
+  contador,
+});
+
+const store = configureStore({
+  reducer,
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export default store;
